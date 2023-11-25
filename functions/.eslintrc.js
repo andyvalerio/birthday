b84c53a35529c71a913f5843c6d133e635907151
Andy Valerio <andy.valerio@astrazeneca.com>
2023-11-25 12:42:50 +0100

    Moved from Typescript to NodeJS

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,71 +1,28 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
     node: true,
   },
-  extends: [
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-  ],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
+    "ecmaVersion": 2018,
   },
-  plugins: [
-    "@typescript-eslint",
-    "import",
+  extends: [
+    "eslint:recommended",
+    "google",
   ],
   rules: {
-    "@typescript-eslint/adjacent-overload-signatures": "error",
-    "@typescript-eslint/no-empty-function": "error",
-    "@typescript-eslint/no-empty-interface": "warn",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-namespace": "error",
-    "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    "@typescript-eslint/prefer-for-of": "warn",
-    "@typescript-eslint/triple-slash-reference": "error",
-    "@typescript-eslint/unified-signatures": "warn",
-    "comma-dangle": "warn",
-    "constructor-super": "error",
-    eqeqeq: ["warn", "always"],
-    "import/no-deprecated": "warn",
-    "import/no-extraneous-dependencies": "error",
-    "import/no-unassigned-import": "warn",
-    "no-cond-assign": "error",
-    "no-duplicate-case": "error",
-    "no-duplicate-imports": "error",
-    "no-empty": [
-      "error",
-      {
-        allowEmptyCatch: true,
-      },
-    ],
-    "no-invalid-this": "error",
-    "no-new-wrappers": "error",
-    "no-param-reassign": "error",
-    "no-redeclare": "error",
-    "no-sequences": "error",
-    "no-shadow": [
-      "error",
-      {
-        hoist: "all",
-      },
-    ],
-    "no-throw-literal": "error",
-    "no-unsafe-finally": "error",
-    "no-unused-labels": "error",
-    "no-var": "warn",
-    "no-void": "error",
-    "prefer-const": "warn",
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
   },
-  settings: {
-    jsdoc: {
-      tagNamePreference: {
-        returns: "return",
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
       },
+      rules: {},
     },
-  },
+  ],
+  globals: {},
 };
